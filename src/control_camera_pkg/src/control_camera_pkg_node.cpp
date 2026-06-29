6430e4b1ad2188456f6218efb2adf492471a664c
dmitry <dimkahs658@gmail.com>
2026-06-29 17:36:29 +0300

    the main skeleton of the program has been added - the publisher-subscriber mechanism

--- src/control_camera_pkg/src/control_camera_pkg_node.cpp
+++ src/control_camera_pkg/src/control_camera_pkg_node.cpp
@@ -2,17 +2,40 @@
 #include <sensor_msgs/msg/image.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
 
 class ControlCamera: public rclcpp::Node {
 public:
     ControlCamera() : Node("control_camera_node") {
 
+        subscriber = this->create_subscription<geometry_msgs::msg::TwistStamped>(
+            "/mavros/local_position/velocity_body",
+            10,  // Глубина очереди (или используйте rclcpp::QoS)
+            std::bind(&ControlCamera::control_camera_callback, this, std::placeholders::_1));
+
+        //Иницилизация публикатора
+        publisher =
+            this->create_publisher<geometry_msgs::msg::TwistStamped>("processed_image",
+                                                            10);
 
 
     }
 
 private:
 
+
+    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr   publisher;
+    rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr subscriber;
+
+
+    geometry_msgs::msg::TwistStamped liner;
+
+    void control_camera_callback(const geometry_msgs::msg::TwistStamped msg){
+
+
+
+
+    }
 
 
 };
